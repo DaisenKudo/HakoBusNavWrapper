c8d7ad2487f48f3d11862a32c59a00d0b6828c73
Squatarola <b1017150@fun.ac.jp>
2022-02-02 23:31:32 +0900

    [mod]Jetpack Composeを導入

--- app/src/main/java/io/github/qlain/hakobusnavwrapper/ui/main/buslocation/request/BusLocationRequestFragment.kt
+++ app/src/main/java/io/github/qlain/hakobusnavwrapper/ui/main/buslocation/request/BusLocationRequestFragment.kt
@@ -5,15 +5,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import io.github.qlain.hakobusnavwrapper.R
 import io.github.qlain.hakobusnavwrapper.ui.buslocationresult.BusLocationResultActivity
 
 class BusLocationRequestFragment : Fragment() {
-
     private lateinit var busLocationRequestViewModel: BusLocationRequestViewModel
 
     companion object {
@@ -22,43 +26,51 @@ class BusLocationRequestFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        busLocationRequestViewModel =
-                ViewModelProvider(this).get(BusLocationRequestViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_bus_location_request, container, false)
-        //val textView: TextView = root.findViewById(R.id.bt_swap)
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        this.busLocationRequestViewModel = ViewModelProvider(this)[BusLocationRequestViewModel::class.java]
+        var etFrom = ""
+        var etTo = ""
 
-        val etFrom = root.findViewById<EditText>(R.id.tv_from).apply {
-
-        }
-        val etTo = root.findViewById<EditText>(R.id.et_to).apply {
-
-        }
-
-        root.findViewById<Button>(R.id.bt_swap).apply {
-            text = busLocationRequestViewModel.buttons["swap"]
-            setOnClickListener {
-                //乗車バス停と降車バス停を入れ替えます
-                val tmp = etFrom.text
-                etFrom.text = etTo.text
-                etTo.text = tmp
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = etFrom,
+                        onValueChange = { etFrom = it },
+                        label = { Text("乗車バス停") },
+                        singleLine = true
+                    )
+                    Button(onClick = { //乗車バス停と降車バス停を入れ替えます
+                        val tmp = etFrom
+                        etFrom = etTo
+                        etTo = tmp
+                    }) {
+                        Text(busLocationRequestViewModel.buttons["swap"]!!)
+                    }
+                    TextField(
+                        value = etTo,
+                        onValueChange = { etTo = it },
+                        label = { Text(text = "降車バス停") },
+                        singleLine = true
+                    )
+                    Button(onClick = {
+                        from = "五稜郭"
+                        to = "函館駅前"
+                        //from = etFrom.text.toString()
+                        //to = etTo.text.toString()
+                        startActivity(Intent(activity, BusLocationResultActivity::class.java))
+                    }) {
+                        Text(busLocationRequestViewModel.buttons["confirm"]!!)
+                    }
+                }
             }
         }
-
-        root.findViewById<Button>(R.id.bt_confirm).apply {
-            text = busLocationRequestViewModel.buttons["confirm"]
-            setOnClickListener {
-                from = "五稜郭"
-                to = "函館駅前"
-                /*from = etFrom.text.toString()
-                to = etTo.text.toString()*/
-                startActivity(Intent(activity, BusLocationResultActivity::class.java))
-            }
-        }
-
-        return root
     }
+
 }
